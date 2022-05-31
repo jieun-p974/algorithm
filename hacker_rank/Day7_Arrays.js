7948b77b332fcd5b683bbf7126536c3fc70fe364
jieun-p974 <jieun.p974@gmail.com>
2022-05-31 16:59:20 +0900

    hacker_rank_day7

---
+++ hacker_rank/Day7_Arrays.js
@@ -0,0 +1,36 @@
+/*
+주어진 배열을 역순으로 출력하면서 띄워쓰기로 구분
+*/
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let arr2 = arr.reverse().join(' ');
+    console.log(arr2);
+
+}
