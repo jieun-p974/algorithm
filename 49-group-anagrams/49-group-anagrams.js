bc9c88063f14d75ae7187d54df947e1916bb1cad
jieun-p974 <84063843+jieun-p974@users.noreply.github.com>
2022-09-16 17:29:04 +0900

    Time: 246 ms (25.01%), Space: 52.8 MB (73.56%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    // Sorting
+    const strSort = strs.map(word => word.split('').sort().join(''));
+    // Create HashMap
+    const hashMap = {};
+
+    // Iterate with normal For-Loop
+    for (let i = 0; i < strs.length; i++) {
+        if (!hashMap[strSort[i]]){
+            hashMap[strSort[i]] = [strs[i]];
+        } else {
+            hashMap[strSort[i]].push(strs[i])
+        }
+    }
+    return Object.values(hashMap)
+};
