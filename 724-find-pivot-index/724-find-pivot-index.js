6659bae42a313ea8703f87f398ccb11cfeda73d1
jieun-p974 <84063843+jieun-p974@users.noreply.github.com>
2022-09-19 21:04:52 +0900

    Time: 550 ms (23.40%), Space: 62 MB (5.06%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let leftSum = 0;
+    let rightSum = nums.reduce((a,c)=>a+c)-nums[0];
+
+    if(leftSum === rightSum) return 0;
+
+    for(i=1; i<nums.length; i++){
+        rightSum-=nums[i];
+        leftSum+=nums[i-1];
+console.log(leftSum, rightSum)
+        if(leftSum === rightSum) return i;
+    }
+    return -1;
+};
